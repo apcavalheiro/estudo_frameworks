b53b2a86f299ddb98c8b38c00e0063b6f188209e
apcavalheiro <adrianocavalheiro1@gmail.com>
2024-05-27 16:03:51 -0300

    Update install_dependencies.ps1

--- install_dependencies.ps1
+++ install_dependencies.ps1
@@ -1,4 +1,4 @@
-# Script PowerShell para automatizar a instalação de AWS CLI, Node.js, Pulumi e Python no Windows 11
+# Script PowerShell para automatizar a instalação de AWS CLI, Node.js, Pulumi, Python, AWS SAM CLI e Serverless Framework no Windows 11
 
 # Função para verificar se um comando está disponível
 function CommandExists {
@@ -41,13 +41,19 @@ if (-not ($env:Path -like "*$nodePath*")) {
 if (-not (CommandExists "pulumi")) {
     Write-Output "Instalando Pulumi..."
     Invoke-WebRequest -Uri "https://get.pulumi.com/releases/sdk/pulumi-v3.74.0-windows-x64.zip" -OutFile "pulumi.zip"
-    Expand-Archive -Path "pulumi.zip" -DestinationPath "C:\Pulumi"
-    $env:Path += ";C:\Pulumi"
-    [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    Expand-Archive -Path "pulumi.zip" -DestinationPath "C:\Pulumi" -Force
+    $pulumiPath = "C:\Pulumi\pulumi"
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Pulumi", [System.EnvironmentVariableTarget]::Machine)
     Remove-Item -Path "pulumi.zip"
     Write-Output "Pulumi instalado."
 } else {
     Write-Output "Pulumi já está instalado."
+}
+
+# Adicionando Pulumi ao Path
+if (-not ($env:Path -like "*$pulumiPath*")) {
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$pulumiPath", [System.EnvironmentVariableTarget]::Machine)
+    Write-Output "Pulumi adicionado ao Path."
 }
 
 # Instalação do Python
@@ -68,7 +74,36 @@ if (-not ($env:Path -like "*$pythonPath*")) {
     Write-Output "Python adicionado ao Path."
 }
 
+# Instalação do AWS SAM CLI
+if (-not (CommandExists "sam")) {
+    Write-Output "Instalando AWS SAM CLI..."
+    Invoke-WebRequest -Uri "https://github.com/aws/aws-sam-cli/releases/latest/download/AWS_SAM_CLI_64_PY3.msi" -OutFile "AWS_SAM_CLI.msi"
+    Start-Process msiexec.exe -ArgumentList '/i AWS_SAM_CLI.msi /quiet' -Wait
+    Remove-Item -Path "AWS_SAM_CLI.msi"
+    Write-Output "AWS SAM CLI instalado."
+} else {
+    Write-Output "AWS SAM CLI já está instalado."
+}
+
+# Adicionando AWS SAM CLI ao Path
+$samPath = "C:\Program Files\Amazon\AWSSAMCLI\bin"
+if (-not ($env:Path -like "*$samPath*")) {
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$samPath", [System.EnvironmentVariableTarget]::Machine)
+    Write-Output "AWS SAM CLI adicionado ao Path."
+}
+
+# Instalação do Serverless Framework
+if (-not (CommandExists "serverless")) {
+    Write-Output "Instalando Serverless Framework..."
+    npm install -g serverless
+    Write-Output "Serverless Framework instalado."
+} else {
+    Write-Output "Serverless Framework já está instalado."
+}
+
 Write-Output "Instalações e configurações concluídas."
+
+
 
 
 #Executando o Script
@@ -77,3 +112,10 @@ Write-Output "Instalações e configurações concluídas."
 #Execute o script com o comando:
 
 #.\install_dependencies.ps1
+
+# aws --version
+# node --version
+# pulumi version
+# python --version
+# sam --version
+# serverless --version
